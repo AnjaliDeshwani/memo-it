c1ddfed25d1548d5b54b99e349b0dda97e92b6f6
AnjaliDeshwani <deshwanianjali@gmail.com>
2022-04-14 01:47:50 +0530

    remove text from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="bg-blue-500 font-medium">
-        Hi, I will make this notes app with speed of light super soon...
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
